13911864e3b60100e6df54dda6ce4ecdc509b197
henry-kanengiser <49458585+henry-kanengiser@users.noreply.github.com>
2024-04-14 13:06:05 -0400

    create boro_5_webmap.R, a script to prepare final analysis files for web mapping

---
+++ pgm/boro_5_webmap.R
@@ -0,0 +1,10 @@
+### Demonstration Analysis
+
+### 5. Output files for web-mapping
+
+# The PURPOSE of this .R script is to save permanent files in a format that is
+# suitable for web mapping using javascript. Most mapping files need to be
+# saved in .geojson formats. This script also saves copies of some descriptive
+# mapping layers as .geojson, including IBZ and BID borders.
+
+# 0. Packages -----------------------------------------------------------------
