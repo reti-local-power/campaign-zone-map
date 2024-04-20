3b4e8de57e563618d5e6032f52c99777917f6d26
henry-kanengiser <49458585+henry-kanengiser@users.noreply.github.com>
2024-04-20 13:59:40 -0400

    set maxzoom and add jsquery code to create a hover state for cz-fill layer

--- js/scripts.js
+++ js/scripts.js
@@ -60,10 +60,17 @@ map.on('load', () => {
     'id': 'cz-fill',
     'type': 'fill',
     'source': 'cz', // reference the data source read in above
+    'maxzoom': 14, // hide fill once the user zooms in enough
     'layout': {},
     'paint': {
       'fill-color': '#54278f',
-      'fill-opacity': 0.4
+      // use a case expression to set the opacity of a polygon based on featureState
+      'fill-opacity': [
+        'case',
+        ['boolean', ['feature-state', 'hover'], false],
+        1,  // opacity when hover is false
+        0.4 // opacity when hover is true
+      ]
     }
   }, 'waterway-label');
 
@@ -183,8 +190,62 @@ map.on('load', () => {
   map.setLayoutProperty('bid-line', 'visibility', 'none');
   map.setLayoutProperty('bid-fill', 'visibility', 'none');
 
+  // this is a variable to store the id of the feature that is currently being hovered.
+  let hoveredPolygonId = null
+
+
+  // whenever the mouse moves on the 'cz-fill' layer, we check the id of the feature it is on 
+  //  top of, and set featureState for that feature.  The featureState we set is hover:true or hover:false
+  map.on('mousemove', 'cz-fill', (e) => {
+    // don't do anything if there are no features from this layer under the mouse pointer
+    if (e.features.length > 0) {
+      // if hoveredPolygonId already has an id in it, set the featureState for that id to hover: false
+      if (hoveredPolygonId !== null) {
+        map.setFeatureState(
+          { source: 'cz', id: hoveredPolygonId },
+          { hover: false }
+        );
+      }
+
+      // set hoveredPolygonId to the id of the feature currently being hovered
+      hoveredPolygonId = e.features[0].id;
+
+      // set the featureState of this feature to hover:true
+      map.setFeatureState(
+        { source: 'cz', id: hoveredPolygonId },
+        { hover: true }
+      );
+
+      // make the cursor a pointer to let the user know it is clickable
+      map.getCanvas().style.cursor = 'pointer'
+
+      // resets the feature state to the default (nothing is hovered) when the mouse leaves the 'borough-boundaries-fill' layer
+      map.on('mouseleave', 'cz-fill', () => {
+        // set the featureState of the previous hovered feature to hover:false
+        if (hoveredPolygonId !== null) {
+          map.setFeatureState(
+            { source: 'cz', id: hoveredPolygonId },
+            { hover: false }
+          );
+        }
+
+        // clear hoveredPolygonId
+        hoveredPolygonId = null;
+
+        // set the cursor back to default
+        map.getCanvas().style.cursor = ''
+      });
+
+    }
+  });
+
 
 });
+
+//// Set hover state for campaign zones to signal they can be clicked
+
+
+
 
 //// Create clickable menu of layers
 
@@ -205,10 +266,10 @@ $('#bid-button').on('click', function () {
     map.setLayoutProperty('bid-fill', 'visibility', 'visible');
   } else {
     map.setLayoutProperty('bid-line', 'visibility', 'none');
-    map.setLayoutProperty('bid-fill', 'visibility', 'none');    
+    map.setLayoutProperty('bid-fill', 'visibility', 'none');
   }
 
-  
+
 })
 
 $('#ibz-button').on('click', function () {
@@ -225,7 +286,7 @@ $('#ibz-button').on('click', function () {
     map.setLayoutProperty('ibz-fill', 'visibility', 'visible');
   } else {
     map.setLayoutProperty('ibz-line', 'visibility', 'none');
-    map.setLayoutProperty('ibz-fill', 'visibility', 'none');    
+    map.setLayoutProperty('ibz-fill', 'visibility', 'none');
   }
 })
 
@@ -241,7 +302,7 @@ $('#dac-button').on('click', function () {
   if (currentvisibility === 'none') {
     map.setLayoutProperty('dac-fill', 'visibility', 'visible');
   } else {
-    map.setLayoutProperty('dac-fill', 'visibility', 'none');    
+    map.setLayoutProperty('dac-fill', 'visibility', 'none');
   }
 })
 
