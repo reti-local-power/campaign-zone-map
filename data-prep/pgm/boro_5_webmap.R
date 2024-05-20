cab948023933bb1fd44ae4a0d2e2c89fa8eede8d
henry-kanengiser <49458585+henry-kanengiser@users.noreply.github.com>
2024-05-20 11:55:16 -0400

    update boro_5_webmap to use new CZ shapes and to add council districts & community districts

--- data-prep/pgm/boro_5_webmap.R
+++ data-prep/pgm/boro_5_webmap.R
@@ -25,7 +25,7 @@ tmap_options(check.and.fix = TRUE)
 bldg <- st_read("dat/suitability index/boro_analysis.shp")
 
 # campaign zone files
-cz <- st_read("dat/boro_Heatmap/campaignzones_halfmile_simple.shp")
+cz <- st_read("dat/boro_Heatmap/campaign_zones_RETIsites.geojson")
 
 cz_data <- read_csv("cz summary statistics.csv") %>%
   filter(!cz_num %in% c("All", "Not in a CZ")) %>%
@@ -46,6 +46,16 @@ ibz <- st_read(ibz_temp2)
 bid <- st_read("https://data.cityofnewyork.us/resource/7jdm-inj8.geojson")
 
 
+# Community Districts
+cd <- st_read("https://data.cityofnewyork.us/resource/jp9i-3b7y.geojson") %>%
+  filter(str_sub(boro_cd, 1, 1) == "3") #restrict to just Brooklyn CDs
+
+# City Council Districts
+council <- st_read("https://data.cityofnewyork.us/resource/s2hu-y8ab.geojson") %>%
+  #filter to Brooklyn Council Districts
+  filter(as.numeric(coun_dist) > 33 &
+           as.numeric(coun_dist) < 49)
+
 
 # 2. Simplify and clean up datasets -------------------------------------------
 
@@ -61,6 +71,7 @@ bldg2 <- bldg %>%
   distinct(bin, .keep_all = T)
 
 # need to add centroid lat and lon for each building
+## this is necessary for constructing the request for streetview image
 bldg_centroid <- bldg2 %>%
   st_centroid() %>%
   st_transform(st_crs(4326)) %>%
@@ -84,30 +95,23 @@ bldg3 <- bldg2 %>%
 
 cz2 <- cz %>%
   mutate(campzone = case_when(
-    fid == "1"  ~ "Greenpoint IBZ",
-    fid == "2"  ~ "North Brooklyn Waterfront",
-    fid == "3"  ~ "Flushing Ave/North Brooklyn IBZ",
-    fid == "4"  ~ "Fort Greene/BK Navy Yard",
-    fid == "5"  ~ "Red Hook",
-    fid == "6"  ~ "East New York IBZ",
-    fid == "7"  ~ "Crown Heights - Utica Ave",
-    fid == "8"  ~ "East New York - Flatlands IBZ",
-    fid == "9"  ~ "Canarsie - Flatlands IBZ",
-    fid == "10" ~ "Sunset Park",
-    fid == "11" ~ "Prospect Park South",
-    fid == "12" ~ "Sheepshead Bay - Nostrand Houses",)) %>%
+    fid == 1  ~ "Greenpoint IBZ",
+    fid == 2  ~ "North Brooklyn Waterfront",
+    fid == 3  ~ "Downtown BK/Naby Yard/North Brooklyn IBZ",
+    fid == 4  ~ "Red Hook/Gowanus",
+    fid == 5  ~ "East New York IBZ",
+    fid == 6  ~ "East New York - Flatlands IBZ",
+    fid == 7  ~ "Canarsie - Flatlands IBZ",
+    fid == 8  ~ "Starrett City",
+    fid == 9 ~ "Prospect Park South",
+    fid == 10  ~ "Sunset Park",
+    fid == 11 ~ "Sheepshead Bay - Nostrand Houses",
+  )) %>%
   select(cz_num = fid, campzone, geometry) %>%
   left_join(cz_data, by = "cz_num") %>%
   #transform to WSG 84 lat/lon information
   st_transform(st_crs(4326)) %>%
   clean_names()
-
-  
-
-# # check that numbering matches
-# tm_shape(cz2) + 
-#   tm_fill("purple")
-
 
 ibz2 <- ibz %>%
   clean_names() %>%
@@ -120,7 +124,6 @@ ibz2 <- ibz %>%
 # # check that the list is comprehensive 
 # tm_shape(ibz2) + 
 #   tm_fill("grey30")
-
 
 bid2 <- bid %>%
   filter(f_all_bi_1 == "Brooklyn") %>%
@@ -142,3 +145,12 @@ st_write(ibz2, "dat/for-web-map/ibz.geojson", delete_dsn = T)
 # bid file
 st_write(bid2, "dat/for-web-map/bid.geojson", delete_dsn = T)
 
+# cd file
+cd %>% 
+  select(-starts_with("shape_")) %>%
+  st_write("dat/for-web-map/cd.geojson", delete_dsn = T)
+
+# council file
+council %>%
+  select(-starts_with("shape_")) %>%
+  st_write("dat/for-web-map/council.geojson", delete_dsn = T)
